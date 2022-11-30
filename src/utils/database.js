d1a77062495413a1ef8bbbd98eabe1048b3abfb5
julio alberto perez <piyoyo279@gmail.com>
2022-11-30 15:22:03 -0500

    API-shoppingcart

--- src/utils/database.js
+++ src/utils/database.js
@@ -5,7 +5,7 @@ const db = new Sequelize({
   database: process.env.DB_NAME || "carrito",
   username: process.env.DB_USER || "julio",
   host: process.env.DB_HOST || "localhost",
-  host: process.env.DB_PORT || 5432,
+  port: process.env.DB_PORT || 5432,
   password: process.env.DB_PASSWORD || 120786,
   dialect: "postgres",
   logging: false,
